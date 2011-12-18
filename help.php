eb707be5d3d764d72f9ea67118b9fd15e037f76a
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-18 06:34:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/onclick-show-popup/trunk@477022 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,9 @@
+<h2>Plugin Management!</h2>
+Click "Go to - Content Management" button to add/update/delete the popup content. <br />
+Click "Go to - Popup Setting" button to update popup setting.
+<h2>How to use!</h2>
+<a target="_blank" href='http://www.gopiplus.com/work/2011/12/17/wordpress-plugin-onclick-show-popup-for-content/'>We have three option check this link</a>
+<h2>About Plugin!</h2>
+Check official website for live demo and more information <a target="_blank" href='http://www.gopiplus.com/work/2011/12/17/wordpress-plugin-onclick-show-popup-for-content/'>www.gopiplus.com</a>.<br />
+<br />
+
